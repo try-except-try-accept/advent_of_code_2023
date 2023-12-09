7e2baf61db249c1f23ca0e9c7d668f0476220b5b
MrHallKellett <chall@kellettschool.com>
2023-12-09 11:15:21 +0800

    day 7 - card game wildcards, complete

--- day7b.py
+++ day7b.py
@@ -33,14 +33,14 @@ class Hand:
         
         #print(self.cards)
         self.bid = int(data[1])
-        five_kind, four_kind, three_kind = None, None, None
-        full_house = None
-        pairs, two_pairs = None, None
+        five_kind, four_kind, three_kind = False, False, False
+        full_house = False
+        pairs, two_pairs = False, False
         high = max(self.cards)
 
-        jokers = self.cards.count(-1)
-        
-        freqs = Counter(self.cards).most_common()
+        jokers = self.cards.count(-1)        
+        freqs = Counter(card for card in self.cards if card > -1).most_common()
+
         try:
             most_common_freq = freqs[0][1]
             most_common_card = freqs[0][0]
@@ -49,34 +49,31 @@ class Hand:
 
             most_common_freq += jokers
             
-            if most_common_freq >= 5:
-                five_kind = most_common_card
-            
-            elif most_common_freq >= 4:
-                four_kind = most_common_card
-            
-            elif most_common_freq >= 3:
+            if most_common_freq == 5:
+                five_kind = True            
+            elif most_common_freq == 4:
+                four_kind = True            
+            elif most_common_freq == 3:
                 jokers_left = (3 - most_common_freq)
-                if sec_most_common_freq >= 2 - jokers_left:
-                    full_house = (most_common_card, sec_most_common_card)
+                if sec_most_common_freq == 2 - jokers_left:
+                    full_house = True
                 else:
-                    three_kind = most_common_card
-            
-            elif most_common_freq >= 2 - jokers:
+                    three_kind = True            
+            elif most_common_freq == 2:
                 jokers_left = (2 - most_common_freq)
-                if sec_most_common_freq >= 2 - jokers_left:
-                    two_pairs = (most_common_card, sec_most_common_card)
+                if sec_most_common_freq == 2 - jokers_left:
+                    two_pairs = True
                 else:
-                    pairs = (most_common_card,)
+                    pairs = True
                     
         except IndexError:
-            five_kind = most_common_card
+            five_kind = True
 
         self.stats = {"5":five_kind,
                       "4":four_kind,
                       "f":full_house,
                       "3":three_kind,
-                      "pp":two_pairs,
+                      "P":two_pairs,
                       "p":pairs,
                       "h":high}
 
@@ -125,27 +122,32 @@ def solve(data):
 
 def tests():
 
-    test_cases = {"5":"77777",
-                  "5J":"7777J",
-                  "5JJ":"777JJ",
-                  "5JJJ":"77JJJ",
-                  "5JJJJ":"7JJJJ",
-                  "5JJJJJ":"JJJJJ",
-                  "4":"77776",
-                  "4J":"777J6",
-                  "4JJ":"77JJ6".
-                  "4JJJ","7JJJ6",
-                  "3JJ":"765JJ",
-                  "3":"77765",
-                  "f":"77766",
-                  "p":"77456",
-                  "pj":"J7654",
-                  "pp":"77665"}
+    test_cases = {"5":"77777 999",
+                  "5J":"7777J 999",
+                  "5JJ":"777JJ 999",
+                  "5JJJ":"77JJJ 999",
+                  "5JJJJ":"7JJJJ 999",
+                  "5JJJJJ":"JJJJJ 999",
+                  "4":"77776 999",
+                  "4J":"777J6 999",
+                  "4JJ":"77JJ6 999",
+                  "4JJJ":"7JJJ6 999",
+                  "3JJ":"765JJ 999",
+                  "3":"77765 999",
+                  "f":"77766 999",
+                  "p":"77456 999",
+                  "pJ":"J7654 999",
+                  "P":"77665 999"}
 
     for win, hand in test_cases.items():
+        
         print(f"Testing {hand}, should get {win[0]}")
-        print(Hand(hand).stats)
-        print("Win found", win[0])
+        stats = Hand(hand).stats
+        print(stats)
+        
+        print()
+        
+        assert stats[win[0]]
 
 
 if __name__ == "__main__":
